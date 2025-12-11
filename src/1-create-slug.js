d8c566d0d4ff2ce943bdfe1aaebe774b2dd18de2
melshabazz2 <melshabazz2@gmail.com>
2025-12-11 14:25:22 -0500

    completed

--- src/1-create-slug.js
+++ src/1-create-slug.js
@@ -1,7 +1,17 @@
 // Return a URL-friendly "slug": lowercase with hyphens instead of spaces.
 // Return null if the title contains banned characters: "!", "#", "?"
 const createSlug = (title) => {
+  const trimmyNeutron = title.trim()
+  const bannedCharacters = ["!", "#", "?"]
 
+  for (let char of bannedCharacters) {
+    if (trimmyNeutron.includes(char)) {
+      return null
+    }
+  }
+  const lowerCaseTitle = trimmyNeutron.toLowerCase()
+  const slug = lowerCaseTitle.split(" ").join("-")
+  return slug
 };
 
 module.exports = {
